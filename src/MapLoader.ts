60b35c706a43bdfc89ce06f13c029552b8136936
granacik320 <tomekkuzniak79@gmail.com>
2025-03-31 18:42:00 +0200

    hotfix

--- src/MapLoader.ts
+++ src/MapLoader.ts
@@ -43,7 +43,7 @@ interface NitroSpawnConfig {
 }
 
 interface WeatherConfig {
-    initialWeather?: string;
+    initialWeather?: WeatherType;
     minDuration?: number;
     maxDuration?: number;
     intensity?: number;
